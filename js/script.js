a15963c486311aee08c7723c170e6d50f6efced3
HenoL-rgb <sasha.diretic@gmail.com>
2022-12-18 00:54:12 +0300

    finished work

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 const list = document.querySelectorAll('li');
-
-list.forEach(item => {
-    item.addEventListener('click', function() {
-            item.classList.toggle('active');
-    })
-})
-
+const form = document.querySelector('.blur');
+const login = document.querySelector('.login');
+const body = document.querySelector("body");
 
 
 document.querySelector('.header__userProfile').addEventListener('click',function(e) {
@@ -15,8 +11,24 @@ document.querySelector('.header__userProfile').addEventListener('click',function
 
 document.querySelector('.popupMenu').addEventListener('click', function(e) {
     e.preventDefault();
-    
+    form.classList.add('active');
+    login.addEventListener('click', function(e) {
+        e.stopPropagation();
+    })
+    form.addEventListener('click', function() {
+        form.classList.remove('active');
+    })
 });
+
+list.forEach(item => {
+    item.addEventListener('click', function() {
+            item.classList.toggle('active');
+    })
+})
+
+
+
+
 
 const auctions = document.querySelectorAll('.content__auctions__cards__card');
 
@@ -32,4 +44,16 @@ const searchBtn = document.querySelector('.header__searchBox__searchBtn');
 
 searchBtn.addEventListener('click', function() {
     window.location.replace('./search.html')
+
+})
+
+
+const hamBtn = document.querySelector('.hamBtn');
+const hamMenu = document.querySelector('.hamburger');
+const closeHam = document.querySelector('.closeHam');
+hamBtn.addEventListener('click', function(e) {
+    hamMenu.classList.toggle('active');
+    closeHam.addEventListener('click', function() {
+        hamMenu.classList.remove('active');
+    })
 })
--- js/stats.js
+++ js/stats.js
@@ -20,3 +20,24 @@ const searchBtn = document.querySelector('.header__searchBox__searchBtn');
 searchBtn.addEventListener('click', function() {
     window.location.replace('./search.html')
 })
+
+const form = document.querySelector('.blur');
+const login = document.querySelector('.login');
+const body = document.querySelector("body");
+
+
+document.querySelector('.header__userProfile').addEventListener('click',function(e) {
+    document.querySelector('.popupMenu').classList.toggle('active');
+
+})
+
+document.querySelector('.popupMenu').addEventListener('click', function(e) {
+    e.preventDefault();
+    form.classList.add('active');
+    login.addEventListener('click', function(e) {
+        e.stopPropagation();
+    })
+    form.addEventListener('click', function() {
+        form.classList.remove('active');
+    })
+});
