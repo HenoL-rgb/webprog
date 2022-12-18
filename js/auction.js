dd64a378900016ccccc578562fbc52b6bf279d8b
HenoL-rgb <sasha.diretic@gmail.com>
2022-12-18 16:12:25 +0300

    s

--- js/auction.js
+++ js/auction.js
@@ -6,6 +6,9 @@ searchBtn.addEventListener('click', function() {
     window.location.replace('./search.html')
 })
 
+
+const b = 'testgit';
+const test = 'dawawdawd'
 const form = document.querySelector('.blur');
 const login = document.querySelector('.login');
 const body = document.querySelector("body");
@@ -15,6 +18,7 @@ document.querySelector('.header__userProfile').addEventListener('click',function
     document.querySelector('.popupMenu').classList.toggle('active');
 
 })
+
 
 document.querySelector('.popupMenu').addEventListener('click', function(e) {
     e.preventDefault();
